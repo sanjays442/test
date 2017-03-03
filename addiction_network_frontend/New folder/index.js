4867f8c0029b0b79fdd02176cc074603e9189c00
unknown <Sanjay Singh>
2017-03-03 18:45:30 +0530

    Removing files

--- addiction_network_frontend/New folder/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-  app = express();
-
-app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/build'));
-
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
-});
