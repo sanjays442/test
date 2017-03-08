3d7578b3fb199b49fe3b0b27e58d2a06aecf9766
unknown <Sanjay Singh>
2017-03-08 19:49:11 +0530

    mapfix testing

--- app/home/index.js
+++ app/home/index.js
@@ -13,6 +13,19 @@ angular.module(moduleName, ['ngRoute'])
     })
   }])
   .factory('SponsoredListingService', service)
-  .controller('HomeCtrl', ctrl);
+  .controller('HomeCtrl', ctrl)
+  .controller('advertisements', function($scope, $http) {
+	$http({
+		  method: 'GET',
+		  url: 'https://ancient-everglades-10056.herokuapp.com/advertisements/',
+		}).then(function successCallback(response) {
+			//$scope.header_adv_banner = response.data.advertisements.header;
+			//$scope.footer_adv_banner = response.data.advertisements.footer;
+			//$scope.sidebar_adv_banner = response.data.advertisements.side_bar;
+		  }, function errorCallback(response) {
+			  console.log(response);
+			$scope.advertisements = response.statusText;
+		  });
+	});
 
 module.exports = moduleName;
