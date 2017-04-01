804e68f3d4dfc8ff068d9ae222bba3f570d81397
Sanjay Singh <sanjays442@gmail.com>
2017-04-02 01:17:35 +0530

    hotFix

--- app/myProfile/myTreatmentCenters/ctrl.js
+++ app/myProfile/myTreatmentCenters/ctrl.js
@@ -21,7 +21,11 @@ function ctrl($log, $rootScope, Status, service, $uibModal) {
       template: '<div class="modal-header"><h3 class="modal-title" id="modal-title">Delete Treatment Center!</h3></div><div class="modal-body" id="modal-body">Are you sure you want to delete?</div><div class="modal-footer"><button class="btn btn-primary" type="button" ng-click="ok()"> OK </button><button class="btn btn-warning" type="button" ng-click="cancel()">Cancel</button ></div>',
       appendTo: parentElem,
       controllerAs: '$ctrl',
-      controller: function ($scope) {},
+      controller: function () {
+        $rootScope.ok = function () {
+          alert('this is tesging');
+        }
+      },
       // controller: 'ModalController',
       resolve: {
         items: function () {
