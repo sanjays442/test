f66fc3b90e9d3764f2dab20754c0f8bfb5fc75b6
Amitabh <sanjays442@gmail.com>
2018-05-21 17:21:02 +0530

    test5 minify module

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 var express = require('express'),
   app = express();
 
-// var minify = require('express-minify');
+var minify = require('express-minify');
 
 app.set('port', (process.env.PORT || 3003));
-//app.use(compression());
-//app.use(minify());
-// app.use(minify({
-//   cache: true,
-//   // uglifyJsModule: null,
-//   // errorHandler: null,
-//   // jsMatch: /javascript/,
-//   // cssMatch: /css/,
-//   // jsonMatch: /json/,
-//   // sassMatch: /scss/,
-//   // lessMatch: /less/,
-//   // stylusMatch: /stylus/,
-//   // coffeeScriptMatch: /coffeescript/,
-// }));
+app.use(compression());
+// app.use(minify());
+app.use(minify({
+  cache: false,
+  // errorHandler: null,
+  jsMatch: /javascript/,
+  cssMatch: /css/,
+  jsonMatch: /json/,
+
+  // sassMatch: /scss/,
+  // lessMatch: /less/,
+  // stylusMatch: /stylus/,
+  // coffeeScriptMatch: /coffeescript/,
+}));
 app.use(express.static(__dirname + '/build'));
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
